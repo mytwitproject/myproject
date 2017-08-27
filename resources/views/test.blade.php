ec8aef27ed2c51e8f2ba996ca1bbb75ced8ae2c4
Tina Re <tina.re1374@yahoo.com>
2017-08-27 18:05:19 +0430

    first

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,14 +3,14 @@
 
 foreach($key as $response=>$val){
     $string = $val->text;
-if (preg_match('/(RT @)(.*?)/', $string, $display) === 1) {
-    //echo $display[1];
-    print_r($display);
-}
-
-//$result = explode('http', $result);
-   // echo  $result[0];
-     //echo  $result[1];
+//if (preg_match('/^[^:]*:/g', $string, $display) === 1) {
+  //  echo $display[1];
+   // print_r($display);
+//}
+$result = preg_replace("/RT/", " ", $string);
+$result = explode('http', $result);
+    echo  $result[0];
+    // echo  $string;
             ?>
 <br>
 <br>
