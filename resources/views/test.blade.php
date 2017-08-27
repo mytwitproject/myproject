2d40ab88a2a052609139fb8cb2df7810bcf4328d
Tina Re <tina.re1374@yahoo.com>
2017-08-27 18:52:02 +0430

    first

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,7 +2,11 @@
 
 
 foreach($key as $response=>$val){
-    $string = $val->text;
+    if(($val->retweet_count)>0){
+        $string = $val->retweeted_status->text;
+        }
+        else
+            $string = $val->text;
 //if (preg_match('/^[^:]*:/g', $string, $display) === 1) {
   //  echo $display[1];
    // print_r($display);
