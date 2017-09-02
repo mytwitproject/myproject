97987511a8a7e6d751a5aee85ecd92395c386b3c
JafarAkhondali <jafar.akhondali@yahoo.com>
2017-09-02 21:35:39 +0430

    Added new cards

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,124 +1,151 @@
 @extends("layouts.master")
 <html>
 <head>
-<style>
-    @section("content_css")
+    <style>
+        @section("content_css")
     html {
-        font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
-        font-size: 14px;
-    }
+            font-family: Lato, 'Helvetica Neue', Arial, Helvetica, sans-serif;
+            font-size: 14px;
+        }
 
-    h5 {
-        font-size: 1.28571429em;
-        font-weight: 700;
-        line-height: 1.2857em;
-        margin: 0;
-    }
+        h5 {
+            font-size: 1.28571429em;
+            font-weight: 700;
+            line-height: 1.2857em;
+            margin: 0;
+        }
 
-    .card {
-        font-size: 1em;
-        overflow: hidden;
-        padding: 0;
-        border: none;
-        border-radius: .28571429rem;
-        box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
-    }
+        .card {
+            font-size: 1em;
+            overflow: hidden;
+            padding: 0;
+            border: none;
+            border-radius: .28571429rem;
+            -box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
+            display: inline-block;
+            width:100%;
+            transform: translateZ(20px);
+            margin-bottom: 10px;
+        }
 
-    .card-block {
-        font-size: 1em;
-        position: relative;
-        margin: 0;
-        padding: 1em;
-        border: none;
-        border-top: 1px solid rgba(34, 36, 38, .1);
-        box-shadow: none;
-    }
+        .card-holder{
+            background-image: linear-gradient(135deg, #b118ac 0%, #26c7da 100%);
+            padding: 5px 10px;
+            transform-style: preserve-3d;
+            margin: 15px 0;
+            vertical-align: top;
+        }
 
-    .card-img-top {
-        display: block;
-        width: 100%;
-        height: auto;
-    }
+        .card-block {
+            font-size: 1em;
+            position: relative;
+            margin: 0;
+            padding: 1em;
+            border: none;
+            border-top: 1px solid rgba(34, 36, 38, .1);
+            box-shadow: none;
+        }
 
-    .card-title {
-        font-size: 1.28571429em;
-        font-weight: 700;
-        line-height: 1.2857em;
-    }
 
-    .card-text {
-        clear: both;
-        margin-top: .5em;
-        color: rgba(0, 0, 0, .68);
-    }
+        .card-image-post{
+            max-width: 100%;
+        }
 
-    .card-footer {
-        font-size: 1em;
-        position: static;
-        top: 0;
-        left: 0;
-        max-width: 100%;
-        padding: .75em 1em;
-        color: rgba(0, 0, 0, .4);
-        border-top: 1px solid rgba(0, 0, 0, .05) !important;
-        background: #fff;
-    }
+        .card-img-top {
+            /*float: left;*/
+            display: inline-block;
+            width: 25%;
+            margin:20px;
+            height: auto;
+        }
+        .card-title .username{
+            text-align: center;
+            display: inline-block;
+            width: calc( 65% - 40px );
+            margin-left: 3%;
+            font-size: 40px;
+            /*float: left;*/
+        }
 
-    .card-inverse .btn {
-        border: 1px solid rgba(0, 0, 0, .05);
-    }
+        .card-title {
+            font-size: 1.28571429em;
+            font-weight: 700;
+            line-height: 1.2857em;
+        }
 
-    .profile {
-        position: absolute;
-        top: -12px;
-        display: inline-block;
-        overflow: hidden;
-        box-sizing: border-box;
-        width: 25px;
-        height: 25px;
-        margin: 0;
-        border: 1px solid #fff;
-        border-radius: 50%;
-    }
+        .card-text {
+            clear: both;
+            margin-top: .5em;
+            color: rgba(0, 0, 0, .68);
+        }
 
-    .profile-avatar {
-        display: block;
-        width: 100%;
-        height: auto;
-        border-radius: 50%;
-    }
+        .card-footer {
+            font-size: 1em;
+            position: static;
+            top: 0;
+            left: 0;
+            max-width: 100%;
+            padding: .75em 1em;
+            color: rgba(0, 0, 0, .4);
+            border-top: 1px solid rgba(0, 0, 0, .05) !important;
+            background: #fff;
+        }
 
-    .profile-inline {
-        position: relative;
-        top: 0;
-        display: inline-block;
-    }
+        .card-inverse .btn {
+            border: 1px solid rgba(0, 0, 0, .05);
+        }
 
-    .profile-inline ~ .card-title {
-        display: inline-block;
-        margin-left: 4px;
-        vertical-align: top;
-    }
+        .profile {
+            position: absolute;
+            top: -12px;
+            display: inline-block;
+            overflow: hidden;
+            box-sizing: border-box;
+            width: 25px;
+            height: 25px;
+            margin: 0;
+            border: 1px solid #fff;
+            border-radius: 50%;
+        }
 
-    .text-bold {
-        font-weight: 700;
-    }
+        .profile-avatar {
+            display: block;
+            width: 100%;
+            height: auto;
+            border-radius: 50%;
+        }
 
-    .meta {
-        font-size: 1em;
-        color: rgba(0, 0, 0, .4);
-    }
+        .profile-inline {
+            position: relative;
+            top: 0;
+            display: inline-block;
+        }
 
-    .meta a {
-        text-decoration: none;
-        color: rgba(0, 0, 0, .4);
-    }
+        .profile-inline ~ .card-title {
+            display: inline-block;
+            margin-left: 4px;
+            vertical-align: top;
+        }
 
-    .meta a:hover {
-        color: rgba(0, 0, 0, .87);
-    }
-    @endsection
-</style>
+        .text-bold {
+            font-weight: 700;
+        }
+
+        .meta {
+            font-size: 1em;
+            color: rgba(0, 0, 0, .4);
+        }
+
+        .meta a {
+            text-decoration: none;
+            color: rgba(0, 0, 0, .4);
+        }
+
+        .meta a:hover {
+            color: rgba(0, 0, 0, .87);
+        }
+        @endsection
+    </style>
 
 </head>
 
@@ -126,209 +153,42 @@
 
 <body>
 @section("cont")
-<div class="col-md-9">
     <div class="row">
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <h5 class="text-bold">Tawshif Ahsan Khan</h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="meta">
-                        <a href="#">Friends</a>
-                    </div>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <span class="float-right">Joined in 2013</span>
-                    <span><i class=""></i>75 Friends</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <p class="card-block">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam velit quisquam veniam excepturi temporibus inventore corporis dicta sit culpa veritatis placeat earum, dolorum asperiores, delectus dolore voluptatibus, at magnam nobis!
-                </p>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title mt-3">Tawshif Ahsan Khan</h4>
-                    <div class="meta">
-                        <a>Friends</a>
-                    </div>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
+
+        @for($i=0;$i<30;$i++)
+            <div class="col-sm-12 col-md-6" style="vertical-align: top;display: inline-block">
+                <div class="card-holder js-tilt" style="vertical-align:top;display: inline-block">
+                    <div class="card" style="vertical-align: top;display: inline-block">
+                        <div class="card-title">
+                            <img class="img-circle card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
+                            <div class="username">@Folani</div>
+                        </div>
+                        <div class="card-block">
+                            @if(rand(1,10)>6)
+                                <img class="card-image-post" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
+                            @endif
+                            <h4 class="card-title">Tawshif Ahsan Khan</h4>
+                            <div class="card-text">
+                                Tawshif is a web designer living in Bangladesh.
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="card-footer">
-                    <small>Last updated 3 mins ago</small>
-                    <button class="btn btn-secondary float-right btn-sm">show</button>
-                </div>
             </div>
-        </div>
+        @endfor
     </div>
-    <div class="row mb-5">
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-primary ">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <blockquote class="card-blockquote p-3">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-                    <footer>
-                        <small>
-                            Someone famous in <cite title="Source Title">Source Title</cite>
-                        </small>
-                    </footer>
-                </blockquote>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title mt-3">Tawshif Ahsan Khan</h4>
-                    <div class="meta card-text">
-                        <a>Friends</a>
-                    </div>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <small>Last updated 3 mins ago</small>
-                    <button class="btn btn-info float-right btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile profile-inline">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <small>Last updated 3 mins ago</small>
-                    <button class="btn btn-info float-right btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile profile-inline">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-info btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile profile-inline">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-info btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile profile-inline">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-info btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile profile-inline">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-info btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mt-4">
-            <div class="card card-inverse card-info">
-                <img class="card-img-top" src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif">
-                <div class="card-block">
-                    <figure class="profile profile-inline">
-                        <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" class="profile-avatar" alt="">
-                    </figure>
-                    <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                    <div class="card-text">
-                        Tawshif is a web designer living in Bangladesh.
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button class="btn btn-info btn-sm">Follow</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 </body>
 
-<script>$(document).ready(function () {fartscroll();})</script>
+<script>$(document).ready(function () {
+        fartscroll();
+        const tilt = $('.js-tilt').tilt({
+            glare: true,
+            maxGlare: .5,
+            scale: 1.0005
+        });
+    })</script>
 @endsection
 
 
