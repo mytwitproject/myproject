793bb7849929da34d8cc0c7a1e5bac96c06a50cd
JafarAkhondali <jafar.akhondali@yahoo.com>
2017-09-07 15:10:55 +0430

    Improve UI

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,6 @@
     {{Html::style('/css/flags.css')}}
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.bootstrap3.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/emojione@3.1.2/extras/css/emojione.min.css"/>
     {{Html::style('/css/style.css')}}
 
 
@@ -19,7 +18,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.1.21/tilt.jquery.min.js"></script>
     {{Html::script('/js/jquery.dd.min.js')}}
-    <script src="https://cdn.jsdelivr.net/npm/emojione@3.1.2/lib/js/emojione.min.js"></script>
 
     <style>
         .nav-side-menu {
