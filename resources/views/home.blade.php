53d1fc47494cca6f312830d6e1527c71648eeb86
JafarAkhondali <jafar.akhondali@yahoo.com>
2017-09-07 11:01:34 +0430

    add second group

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,26 +7,28 @@
 <body>
 @section("cont")
     <div class="row">
-        @foreach($key as $response=>$val)
-            <div class="col-sm-12 col-md-6" style="vertical-align: top;float: right">
-                <div class="card-holder js-tilt" >
-                    <div class="card">
-                        <div class="card-title">
-                            <img class="img-circle card-img-top" src="<?php echo($val['img_url']); ?>">
-                            <div class="username">{{'@'}}{{$val['user_name']}}</div>
-                        </div>
-                        <div class="card-block">
-                            @if($val['tweet_img']!="null")
-                                <img class="card-image-post" src="<?php echo($val['tweet_img']); ?>">
-                            @endif
-                            <h4 class="card-title">Tawshif Ahsan Khan</h4>
-                            <div class="card-text">
-                                {{$val['full_text']}}
+        @foreach($key as $response=>$group)
+            @foreach($group as $id=>$val)
+                <div class="col-sm-12 col-md-6" style="vertical-align: top;float: right">
+                    <div class="card-holder js-tilt" >
+                        <div class="card">
+                            <div class="card-title">
+                                <img class="img-circle card-img-top" src="<?php echo($val['img_url']); ?>">
+                                <div class="username">{{'@'}}{{$val['user_name']}}</div>
+                            </div>
+                            <div class="card-block">
+                                @if($val['tweet_img']!="null")
+                                    <img class="card-image-post" src="<?php echo($val['tweet_img']); ?>">
+                                @endif
+                                <h4 class="card-title">Tawshif Ahsan Khan</h4>
+                                <div class="card-text">
+                                    {{$val['full_text']}}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         @endforeach
     </div>
 
